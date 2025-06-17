b1209c9c0bcc8dd339bb7e8bf6862de12d0e9093
Igor Tudisco <igortudisco@hotmail.com>
2025-06-17 18:20:29 -0300

    Refactor "show" command to use Show class
    Replaced the previous implementation of the "show" command in `Program.cs` with a new approach that utilizes the `Show` class. The old file reading and display logic has been removed and encapsulated within the `Show` class in `Show.cs`, improving code organization and separation of concerns.

---
+++ Alura.Adopet.Console/Show.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading.Tasks;
+
+namespace Alura.Adopet.Console;
+
+internal class Show
+{
+    public void ShowArquivoImportado(string caminhoDoArquivoAserExibido)
+    {
+        using (StreamReader sr = new StreamReader(caminhoDoArquivoAserExibido))
+        {
+            System.Console.WriteLine("----- Serão importados os dados abaixo -----");
+            while (!sr.EndOfStream)
+            {
+                // separa linha usando ponto e vírgula
+                string[] propriedades = sr.ReadLine().Split(';');
+                // cria objeto Pet a partir da separação
+                Pet pet = new Pet(Guid.Parse(propriedades[0]),
+                propriedades[1],
+                TipoPet.Cachorro
+                );
+                System.Console.WriteLine(pet);
+            }
+        }
+    }
+}
