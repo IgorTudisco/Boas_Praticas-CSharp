cba321fd38590b7226360a651d57638b0916cd9c
Igor Tudisco <igortudisco@hotmail.com>
2025-06-20 14:52:55 -0300

    Add documentation comments to DocComando class
    Enhanced the `DocComando` class in `DocComando.cs` with a block of comments.
    These comments include links to .NET design guidelines, resources for ASP.NET Core and Azure architecture, a GitHub repository for attribute examples, and details about the Swashbuckle library for Swagger UI documentation.

--- Alura.Adopet.Console/DocComando.cs
+++ Alura.Adopet.Console/DocComando.cs
@@ -16,6 +16,18 @@ internal class DocComando: System.Attribute
 }
 
 /*
+ * 
  * Documentação de Atributos (diretrizes de design do .NET Framework):
  * https://learn.microsoft.com/en-us/dotnet/standard/design-guidelines/attributes
+ * 
+ * Arquitete aplicações web modernas com ASP.NET Core e Azure
+ * https://learn.microsoft.com/en-us/dotnet/architecture/modern-web-apps-azure/
+ * 
+ * Repositório no GitHub com exemplos de atributos:
+ * https://github.com/dotnet-architecture/eShopOnWeb
+ * 
+ * Biblioteca Swashbuckle. Essa biblioteca é responsável por olhar para o nosso código, através de reflections,
+ * trazer informações e montar uma página como a Swagger UI da Alura.Adopet.API.
+ * https://www.nuget.org/packages/Swashbuckle.AspNetCore.Swagger
+ * 
  */
