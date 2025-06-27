9d3fba59d75195f608a6b94d09c75c888b6afa73
Igor Tudisco <igortudisco@hotmail.com>
2025-06-27 15:29:03 -0300

    Update IComando.cs with educational resources
    Enhanced documentation for the internal interface `IComando` by adding comments and links related to asynchronous programming and design patterns. Included articles and courses on object-oriented programming and design patterns to provide useful references for developers.

--- Alura.Adopet.Console/Comandos/IComando.cs
+++ Alura.Adopet.Console/Comandos/IComando.cs
@@ -14,3 +14,13 @@ internal interface IComando
  * Modelo de programação assíncrona baseado em tarefas: https://learn.microsoft.com/pt-br/dotnet/csharp/asynchronous-programming/task-asynchronous-programming-model
  * 
  */
+
+/*
+ * Para saber sobre padrões de projeto, acesse os links abaixo:
+ * 
+ * Artigo POO: o que é programação orientada a objetos? https://www.alura.com.br/artigos/poo-programacao-orientada-a-objetos
+ * Artigo Design patterns: Breve introdução aos padrões de projeto; https://www.alura.com.br/artigos/design-patterns-introducao-padroes-projeto
+ * Curso Design Patterns C# I; https://cursos.alura.com.br/course/design-patterns-dotnet
+ * Curso Design Patterns C# II. https://cursos.alura.com.br/course/design-patterns-2-dot-net
+ * 
+ */
