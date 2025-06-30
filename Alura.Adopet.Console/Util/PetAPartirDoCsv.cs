1fac5c92e435125c4f3da1bdbd0a90badb5b7a35
Igor Tudisco <igortudisco@hotmail.com>
2025-06-30 17:12:59 -0300

    Add PetAPartirDoCsv class and corresponding tests
    Introduce the PetAPartirDoCsv class for converting string
    representations of pets into Pet objects. Implement the
    ConverteDoTexto method to handle the conversion logic.
    Add unit tests in PetAPartirDoCsvTest to ensure valid
    string inputs return non-null Pet instances.

---
+++ Alura.Adopet.Console/Util/PetAPartirDoCsv.cs
@@ -0,0 +1,19 @@
+﻿using Alura.Adopet.Console.Modelos;
+
+namespace Alura.Adopet.Console.Util;
+
+public class PetAPartirDoCsv
+{
+    public Pet ConverteDoTexto(string linha)
+    {
+
+        string[] propriedades = linha.Split(';');
+
+        Pet pet = new Pet(Guid.Parse(propriedades[0]),
+        propriedades[1],
+        TipoPet.Cachorro
+        );
+
+        return pet;
+    }
+}
---
+++ Alura.Adopet.Testes/PetAPartirDoCsvTest.cs
@@ -0,0 +1,21 @@
+﻿using Alura.Adopet.Console.Util;
+
+namespace Alura.Adopet.Testes;
+
+public class PetAPartirDoCsvTest
+{
+    [Fact]
+    public void QuandoStringForValidaDeveRetornarUmPet()
+    {
+        // Arrange
+        string linha = "456b24f4-19e2-4423-845d-4a80e8854a41;Lima Limão;1";
+        var conversor = new PetAPartirDoCsv();
+
+
+        // Act
+        var pet = conversor.ConverteDoTexto(linha);
+
+        // Assert
+        Assert.NotNull(pet);
+    }
+}
