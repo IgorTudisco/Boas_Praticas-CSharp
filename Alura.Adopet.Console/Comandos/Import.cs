59b48356d98a6ded18e05769ddafe602ac6c4947
Igor Tudisco <igortudisco@hotmail.com>
2025-06-30 15:49:26 -0300

    Refactor HTTP client usage with HttpClientPet class
    Introduced a new `HttpClientPet` class to centralize HTTP operations for creating and listing pets. Updated the `Import` and `List` classes to utilize `HttpClientPet`, removing their own `HttpClient` instances. Improved command documentation for consistency and removed unnecessary using directives.

--- Alura.Adopet.Console/Comandos/Import.cs
+++ Alura.Adopet.Console/Comandos/Import.cs
@@ -1,36 +1,29 @@
 ﻿using Alura.Adopet.Console.Modelos;
+using Alura.Adopet.Console.Servicos;
 using Alura.Adopet.Console.Util;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.Http.Headers;
-using System.Net.Http.Json;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Alura.Adopet.Console.Comandos;
 
-[DocComando(instrucao: "Import", documentacao: "adopet import <arquivo> comando que realiza a importação do arquivo de pets.")]
-internal class Import: IComando
+[DocComando(instrucao: "import",
+        documentacao: "adopet import <ARQUIVO> comando que realiza a importação do arquivo de pets.")]
+internal class Import : IComando
 {
-    HttpClient client;
-
-    public Import()
+    public async Task ExecutaAsync(string[] args)
     {
-        client = ConfiguraHttpClient("http://localhost:5057");
+        await this.ImportacaoDeArquivoPetAsyc(caminhoDoArquivoDeImportacao: args[1]);
     }
 
     private async Task ImportacaoDeArquivoPetAsyc(string caminhoDoArquivoDeImportacao)
     {
-        var leitorDeArquivo = new LeitorDeArquivo();
-        List<Pet> listaDePet = leitorDeArquivo.RealizaLeitura(caminhoDoArquivoDeImportacao);
-
+        var leitor = new LeitorDeArquivo();
+        List<Pet> listaDePet = leitor.RealizaLeitura(caminhoDoArquivoDeImportacao);
         foreach (var pet in listaDePet)
         {
-            System.Console.WriteLine($"ID: {pet.Id}, Nome: {pet.Nome}, Tipo: {pet.Tipo}");
+            System.Console.WriteLine(pet);
             try
             {
-                var resposta = await CreatePetAsync(pet);
+                var httpCreatePet = new HttpClientPet();
+                await httpCreatePet.CreatePetAsync(pet);
             }
             catch (Exception ex)
             {
@@ -38,30 +31,6 @@ internal class Import: IComando
             }
         }
         System.Console.WriteLine("Importação concluída!");
-    }
-
-    Task<HttpResponseMessage> CreatePetAsync(Pet pet)
-    {
-        HttpResponseMessage? response = null;
-        using (response = new HttpResponseMessage())
-        {
-            return client.PostAsJsonAsync("pet/add", pet);
-        }
-    }
-
-    HttpClient ConfiguraHttpClient(string url)
-    {
-        HttpClient _client = new HttpClient();
-        _client.DefaultRequestHeaders.Accept.Clear();
-        _client.DefaultRequestHeaders.Accept.Add(
-            new MediaTypeWithQualityHeaderValue("application/json"));
-        _client.BaseAddress = new Uri(url);
-        return _client;
-    }
-
-    public async Task ExecutaAsync(string[] args)
-    {
-        await this.ImportacaoDeArquivoPetAsyc(caminhoDoArquivoDeImportacao: args[1]);
     }
 }
 
--- Alura.Adopet.Console/Comandos/List.cs
+++ Alura.Adopet.Console/Comandos/List.cs
@@ -1,4 +1,5 @@
 ﻿using Alura.Adopet.Console.Modelos;
+using Alura.Adopet.Console.Servicos;
 using System.Net.Http.Headers;
 using System.Net.Http.Json;
 
@@ -7,40 +8,19 @@ namespace Alura.Adopet.Console.Comandos;
 [DocComando(instrucao: "List", documentacao: "adopet list comando que exibe no terminal o conteúdo cadastrado na base de dados da AdoPet.")]
 internal class List: IComando
 {
-    HttpClient client;
-
-    public List()
+    private async Task ListaDadosPetsDaAPIAsync()
     {
-        client = ConfiguraHttpClient("http://localhost:5057");
-    }
-
-    private async Task ListDePets()
-    {
-        var pets = await ListPetsAsync();
-        foreach (var pet in pets!)
+        var httpListPet = new HttpClientPet();
+        IEnumerable<Pet>? pets = await httpListPet.ListPetsAsync();
+        System.Console.WriteLine("----- Lista de Pets importados no sistema -----");
+        foreach (var pet in pets)
         {
             System.Console.WriteLine(pet);
         }
     }
 
-    async Task<IEnumerable<Pet>?> ListPetsAsync()
-    {
-        HttpResponseMessage response = await client.GetAsync("pet/list");
-        return await response.Content.ReadFromJsonAsync<IEnumerable<Pet>>();
-    }
-
-    HttpClient ConfiguraHttpClient(string url)
-    {
-        HttpClient _client = new HttpClient();
-        _client.DefaultRequestHeaders.Accept.Clear();
-        _client.DefaultRequestHeaders.Accept.Add(
-            new MediaTypeWithQualityHeaderValue("application/json"));
-        _client.BaseAddress = new Uri(url);
-        return _client;
-    }
-
     public async Task ExecutaAsync(string[] args)
     {
-        await this.ListDePets();
+        await this.ListaDadosPetsDaAPIAsync();
     }
 }
