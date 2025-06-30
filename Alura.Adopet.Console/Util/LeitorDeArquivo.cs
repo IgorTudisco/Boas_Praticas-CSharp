269aa559e533b39cc909ca07bbfa935a874774de
Igor Tudisco <igortudisco@hotmail.com>
2025-06-30 17:23:36 -0300

    Refactor Pet object creation from file input
    Updated the LeitorDeArquivo class to read lines from a file and convert them directly into Pet objects using the new ConverteDoTexto() method. This change simplifies the code by removing manual string splitting and enhances readability and maintainability.

--- Alura.Adopet.Console/Util/LeitorDeArquivo.cs
+++ Alura.Adopet.Console/Util/LeitorDeArquivo.cs
@@ -12,13 +12,8 @@ internal class LeitorDeArquivo
             System.Console.WriteLine("----- Serão importados os dados abaixo -----");
             while (!sr.EndOfStream)
             {
-                // separa linha usando ponto e vírgula
-                string[] propriedades = sr.ReadLine().Split(';');
-                // cria objeto Pet a partir da separação
-                Pet pet = new Pet(Guid.Parse(propriedades[0]),
-                propriedades[1],
-                TipoPet.Cachorro
-                );
+                var linha = sr.ReadLine()!;
+                var pet = linha.ConverteDoTexto();
                 listaDePets.Add(pet);
             }
         }
