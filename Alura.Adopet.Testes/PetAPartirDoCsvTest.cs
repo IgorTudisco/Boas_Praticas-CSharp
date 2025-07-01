3b6fba01c2af56cac651a78964d666839410f5ed
Igor Tudisco <igortudisco@hotmail.com>
2025-07-01 12:13:15 -0300

    Add unit tests for ConverteDoTexto method in PetAPartirDoCsvTest
    This commit introduces several unit tests in `PetAPartirDoCsvTest.cs` to validate the functionality of the `ConverteDoTexto` method. The tests ensure that the `pet` object is correctly instantiated from a string input, checking that it is not null and that its properties (name, ID, and type) are valid and not empty. Additionally, the tests confirm that the ID is a valid GUID and that the pet type matches the expected enum value (`TipoPet.Gato`).

--- Alura.Adopet.Testes/PetAPartirDoCsvTest.cs
+++ Alura.Adopet.Testes/PetAPartirDoCsvTest.cs
@@ -1,4 +1,5 @@
-﻿using Alura.Adopet.Console.Util;
+﻿using Alura.Adopet.Console.Modelos;
+using Alura.Adopet.Console.Util;
 
 namespace Alura.Adopet.Testes;
 
@@ -16,6 +17,71 @@ public class PetAPartirDoCsvTest
         // Assert
         Assert.NotNull(pet);
     }
+
+    [Fact]
+    public void QuandoStringNaoForNulaEVaziaDeveRetornarUmPet()
+    {
+        // Arrange
+        string linha = "456b24f4-19e2-4423-845d-4a80e8854a41;Lima Limão;1";
+
+        // Act
+        var pet = linha.ConverteDoTexto();
+
+        // Assert
+        Assert.NotNull(pet);
+        Assert.NotEmpty(pet.Nome!);
+        Assert.NotEmpty(pet.Id.ToString());
+        Assert.NotEmpty(pet.Tipo.ToString());
+    }
+
+    [Fact]
+    public void VerificaSeOsCamposSaoValidos()
+    {
+        // Arrange
+        string linha = "456b24f4-19e2-4423-845d-4a80e8854a41;Lima Limão;1";
+
+        // Act
+        var pet = linha.ConverteDoTexto();
+
+        // Assert
+        Assert.NotEmpty(pet.Nome!);
+        Assert.NotEmpty(pet.Id.ToString());
+        Assert.NotEmpty(pet.Tipo.ToString());
+        Assert.NotNull(pet.Nome!);
+        Assert.NotNull(pet.Id.ToString());
+        Assert.NotNull(pet.Tipo.ToString());
+    }
+
+    [Fact]
+    public void VerificaSeOGuidValido()
+    {
+        // Arrange
+        string linha = "456b24f4-19e2-4423-845d-4a80e8854a41;Lima Limão;1";
+
+        // Act
+        var pet = linha.ConverteDoTexto();
+
+        // Assert
+        Assert.Equal("Lima Limão", pet.Nome);
+        Assert.Equal(Guid.Parse("456b24f4-19e2-4423-845d-4a80e8854a41"), pet.Id);
+    }
+
+    [Fact]
+    public void VerificaSeOTipoValido()
+    {
+        // Arrange
+        string linha = "456b24f4-19e2-4423-845d-4a80e8854a41;Lima Limão;1";
+
+        // Act
+        var pet = linha.ConverteDoTexto();
+
+        // Assert
+        Assert.Equal(TipoPet.Gato, pet.Tipo);
+        Assert.Equal("Gato", pet.Tipo.ToString());
+
+    }
+
+
 }
 
 
