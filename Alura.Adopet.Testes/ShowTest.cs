1916dcc9d398fc437080de51673c336d251c3802
Igor Tudisco <igortudisco@hotmail.com>
2025-09-16 14:24:01 -0300

    Make Show class public and add unit tests for it
    Updated the `Show` class in `Show.cs` to be public, enabling access from other assemblies. Added `using FluentResults;` to leverage the FluentResults library for improved result handling.
    
    In `ShowTest.cs`, included necessary `using` directives and introduced a new test method `QuandoArquivoExistenteDeveRetornarMensagemDeSucesso`. This method tests the `Show` class functionality with a valid file, ensuring the result is not null and contains a success message.

--- Alura.Adopet.Testes/ShowTest.cs
+++ Alura.Adopet.Testes/ShowTest.cs
@@ -1,5 +1,32 @@
-﻿namespace Alura.Adopet.Testes;
+﻿using Alura.Adopet.Console.Comandos;
+using Alura.Adopet.Console.Modelos;
+using Alura.Adopet.Console.Util;
+using Alura.Adopet.Testes.Builder;
+
+namespace Alura.Adopet.Testes;
 
 public class ShowTest
 {
+    [Fact]
+    public async Task QuandoArquivoExistenteDeveRetornarMensagemDeSucesso()
+    {
+        //Arrange
+        List<Pet> listaDePet = new();
+        var pet = new Pet(new Guid("456b24f4-19e2-4423-845d-4a80e8854a99"),
+                          "Lima", TipoPet.Cachorro);
+        listaDePet.Add(pet);
+        var leitor = LeitorDeArquivosMockBuilder.CriaMock(listaDePet);
+        leitor.Setup(s => s.RealizaLeitura());
+
+        var show = new Show(leitor.Object);
+
+        //Act
+        var resultado = await show.ExecutaAsync();
+
+        //Assert
+        Assert.NotNull(resultado);
+        var sucesso = (SuccessWhithPets)resultado.Successes[0];
+        Assert.Equal("Importação realizada com sucesso!",
+            sucesso.Message);
+    }
 }
